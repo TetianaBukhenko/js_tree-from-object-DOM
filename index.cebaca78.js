26da6d96f45ece95049f6dc9d89fef6aa7db8805
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-08 17:16:45 +0300

    make build

---
+++ index.cebaca78.js
@@ -0,0 +1,2 @@
+!function e(n,r){return Object.keys(r).forEach(function(t){var a=document.createElement("li");if(a.innerText=t,Object.keys(r[t]).length>0){var c=document.createElement("ul");e(c,r[t]),a.append(c)}n.append(a)}),n}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.cebaca78.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,49 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    Object.keys(data).forEach((key)=>{
+        const listItem = document.createElement("li");
+        listItem.innerText = key;
+        if (Object.keys(data[key]).length > 0) {
+            const newList = document.createElement("ul");
+            createTree(newList, data[key]);
+            listItem.append(newList);
+        }
+        element.append(listItem);
+    });
+    return element;
+}
+createTree(tree, food); // function createTree(element, data) {
+ //   const list = document.createElement('ul');
+ //   element.append(list);
+ //   Object.keys(data).forEach((key) => {
+ //     const listItem = document.createElement('li');
+ //     listItem.innerText = key;
+ //     if (Object.keys(data[key])) {
+ //       const newList = document.createElement('ul');
+ //       createTree(newList, data[key]);
+ //       listItem.append(newList);
+ //     }
+ //     element.append(listItem);
+ //   });
+ //   element.insertAdjacentElement('beforeend', list);
+ //   return element;
+ // }
+
+//# sourceMappingURL=index.f75de5e1.js.map
